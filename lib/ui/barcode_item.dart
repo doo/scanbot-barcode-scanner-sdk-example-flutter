05fc147789cf6a67277475bafd41a2f16e68a541
Serhii Chaban <serhii@scanbot.io>
2021-02-18 15:25:01 +0100

    fix crash on msi plessey

--- lib/ui/barcode_item.dart
+++ lib/ui/barcode_item.dart
@@ -45,5 +45,6 @@ const barcodeFormatEnumMap = {
   BarcodeFormat.RSS_EXPANDED: 'RSS_EXPANDED',
   BarcodeFormat.UPC_A: 'UPC_A',
   BarcodeFormat.UPC_E: 'UPC_E',
+  BarcodeFormat.MSI_PLESSEY: 'MSI_PLESSEY',
   BarcodeFormat.UNKNOWN: 'UNKNOWN',
 };
