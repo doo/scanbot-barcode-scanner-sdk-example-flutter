e758d2870a2d0d0e5553724565522df8416c4ba0
yurii-scanbot <yurii@scanbot.io>
2024-05-28 17:42:23 +0200

    update barcode_itemv2 display result with wrapped generic fields

--- lib/ui/V2/barcode_item_v2.dart
+++ lib/ui/V2/barcode_item_v2.dart
@@ -1,3 +1,4 @@
+import 'package:barcode_scanner/json/common_generic_document.dart';
 import 'package:flutter/material.dart';
 import 'package:barcode_scanner/scanbot_barcode_sdk_v2.dart';
 
@@ -24,8 +25,66 @@ class BarcodeItemWidgetV2 extends StatelessWidget {
             child: Text(item.text ?? "",
                 style: const TextStyle(inherit: true, color: Colors.black)),
           ),
+          wrappedGenericDocumentField(),
         ],
       ),
+    );
+  }
+
+  Widget wrappedGenericDocumentField() {
+    if (item.parsedDocument == null) return Container();
+
+    GenericDocumentWrapper documentWrapper =
+        GenericDocumentWrapper.wrap(item.parsedDocument!);
+
+    TextFieldWrapper? wrappedGenericFieldValue;
+
+    switch (documentWrapper.document.type.name) {
+      case BoardingPass.DOCUMENT_TYPE:
+        wrappedGenericFieldValue =
+            (documentWrapper as BoardingPass).ElectronicTicket;
+        break;
+      case SwissQR.DOCUMENT_TYPE:
+        wrappedGenericFieldValue = (documentWrapper as SwissQR).IBAN;
+        break;
+
+      case DEMedicalPlan.DOCUMENT_TYPE:
+        wrappedGenericFieldValue =
+            (documentWrapper as DEMedicalPlan).Doctor.IssuerName;
+        break;
+
+      case IDCardPDF417.DOCUMENT_TYPE:
+        wrappedGenericFieldValue =
+            (documentWrapper as IDCardPDF417).DateExpired;
+        break;
+
+      case GS1.DOCUMENT_TYPE:
+        wrappedGenericFieldValue =
+            (documentWrapper as GS1).Elements?.first.ApplicationIdentifier;
+        break;
+
+      case SEPA.DOCUMENT_TYPE:
+        (documentWrapper as SEPA).ReceiverIBAN;
+        break;
+
+      case MedicalCertificate.DOCUMENT_TYPE:
+        (documentWrapper as MedicalCertificate).DoctorNumber;
+        break;
+
+      case VCard.DOCUMENT_TYPE:
+        (documentWrapper as VCard).FirstName;
+        break;
+
+      case AAMVA.DOCUMENT_TYPE:
+        (documentWrapper as AAMVA).DriverLicense;
+        break;
+    }
+
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Text(
+          "Document: ${documentWrapper.document.type.name} \nField: ${wrappedGenericFieldValue?.type.name} \nValue: ${wrappedGenericFieldValue?.value?.text}",
+          style: const TextStyle(inherit: true, color: Colors.black)),
     );
   }
 }
