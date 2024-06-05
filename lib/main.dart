ef9b10d8c7914779cea16cd7501ff9788daee43d
Serhii Chaban <serhii@scanbot.io>
2024-06-05 15:44:42 +0200

    check memory issue

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,96 @@ void main() => runApp(MyApp());
 // You can get an unrestricted "no-strings-attached" 30 day trial license key for free.
 // Please submit the trial license form (https://scanbot.io/en/sdk/demo/trial) on our website by using
 // the app identifier "io.scanbot.example.sdk.barcode.flutter" of this example app or of your app.
-const BARCODE_SDK_LICENSE_KEY = "";
+const BARCODE_SDK_LICENSE_KEY = "I+MTYRjgxBo4xpGqu32CafPylHNvVK" +
+    "upQLtwyu4IBf4RtChRH6+zH+uB4Xwg" +
+    "/etnl8A3e9w34oApfvgoVT0uUntsH6" +
+    "0z2ZmSAMugTlwZJDDP0K4LLP6XKoMW" +
+    "b9v7x+i6M29bSMo8WQqwosOnfaI4ON" +
+    "y9vrhCqvrN9qx4lIK3TwqkVcxa5hAI" +
+    "Hecsct+YIy0wwrLiXcWc5tJb8Kc9iB" +
+    "7I2MxoG/7k0CCsv14LY48D+Cm/uLLz" +
+    "NSG8aIcYqOXAC9PimxQKcaOqwiP2bV" +
+    "L7Y66cmdnEhD1907OuLyAaAZ87DDH0" +
+    "heHfkkZ+hvNtRB15lAmzrXplWJOgX6" +
+    "Uu/ipnLc+z7Q==\nU2NhbmJvdFNESw" +
+    "pkb28uc2NhbmJvdC5jYXBhY2l0b3Iu" +
+    "ZXhhbXBsZXxpby5zY2FuYm90LmV4YW" +
+    "1wbGUuZmx1dHRlcnxpby5zY2FuYm90" +
+    "LmV4YW1wbGUuc2RrLmFuZHJvaWR8aW" +
+    "8uc2NhbmJvdC5leGFtcGxlLnNkay5i" +
+    "YXJjb2RlLmFuZHJvaWR8aW8uc2Nhbm" +
+    "JvdC5leGFtcGxlLnNkay5iYXJjb2Rl" +
+    "LmNhcGFjaXRvcnxpby5zY2FuYm90Lm" +
+    "V4YW1wbGUuc2RrLmJhcmNvZGUuZmx1" +
+    "dHRlcnxpby5zY2FuYm90LmV4YW1wbG" +
+    "Uuc2RrLmJhcmNvZGUuaW9uaWN8aW8u" +
+    "c2NhbmJvdC5leGFtcGxlLnNkay5iYX" +
+    "Jjb2RlLm1hdWl8aW8uc2NhbmJvdC5l" +
+    "eGFtcGxlLnNkay5iYXJjb2RlLm5ldH" +
+    "xpby5zY2FuYm90LmV4YW1wbGUuc2Rr" +
+    "LmJhcmNvZGUucmVhY3RuYXRpdmV8aW" +
+    "8uc2NhbmJvdC5leGFtcGxlLnNkay5i" +
+    "YXJjb2RlLndpbmRvd3N8aW8uc2Nhbm" +
+    "JvdC5leGFtcGxlLnNkay5iYXJjb2Rl" +
+    "LnhhbWFyaW58aW8uc2NhbmJvdC5leG" +
+    "FtcGxlLnNkay5iYXJjb2RlLnhhbWFy" +
+    "aW4uZm9ybXN8aW8uc2NhbmJvdC5leG" +
+    "FtcGxlLnNkay5jYXBhY2l0b3J8aW8u" +
+    "c2NhbmJvdC5leGFtcGxlLnNkay5jYX" +
+    "BhY2l0b3IuYW5ndWxhcnxpby5zY2Fu" +
+    "Ym90LmV4YW1wbGUuc2RrLmNhcGFjaX" +
+    "Rvci5pb25pY3xpby5zY2FuYm90LmV4" +
+    "YW1wbGUuc2RrLmNhcGFjaXRvci5pb2" +
+    "5pYy5yZWFjdHxpby5zY2FuYm90LmV4" +
+    "YW1wbGUuc2RrLmNhcGFjaXRvci5pb2" +
+    "5pYy52dWVqc3xpby5zY2FuYm90LmV4" +
+    "YW1wbGUuc2RrLmNvcmRvdmEuaW9uaW" +
+    "N8aW8uc2NhbmJvdC5leGFtcGxlLnNk" +
+    "ay5mbHV0dGVyfGlvLnNjYW5ib3QuZX" +
+    "hhbXBsZS5zZGsuaW9zLmJhcmNvZGV8" +
+    "aW8uc2NhbmJvdC5leGFtcGxlLnNkay" +
+    "5pb3MuY2xhc3NpY3xpby5zY2FuYm90" +
+    "LmV4YW1wbGUuc2RrLmlvcy5ydHV1aX" +
+    "xpby5zY2FuYm90LmV4YW1wbGUuc2Rr" +
+    "Lm1hdWl8aW8uc2NhbmJvdC5leGFtcG" +
+    "xlLnNkay5tYXVpLnJ0dXxpby5zY2Fu" +
+    "Ym90LmV4YW1wbGUuc2RrLm5ldHxpby" +
+    "5zY2FuYm90LmV4YW1wbGUuc2RrLnJl" +
+    "YWN0bmF0aXZlfGlvLnNjYW5ib3QuZX" +
+    "hhbXBsZS5zZGsucmVhY3QubmF0aXZl" +
+    "fGlvLnNjYW5ib3QuZXhhbXBsZS5zZG" +
+    "sucnR1LmFuZHJvaWR8aW8uc2NhbmJv" +
+    "dC5leGFtcGxlLnNkay54YW1hcmlufG" +
+    "lvLnNjYW5ib3QuZXhhbXBsZS5zZGsu" +
+    "eGFtYXJpbi5mb3Jtc3xpby5zY2FuYm" +
+    "90LmV4YW1wbGUuc2RrLnhhbWFyaW4u" +
+    "cnR1fGlvLnNjYW5ib3QuZm9ybXMubm" +
+    "F0aXZlcmVuZGVyZXJzLmV4YW1wbGV8" +
+    "aW8uc2NhbmJvdC5uYXRpdmViYXJjb2" +
+    "Rlc2RrcmVuZGVyZXJ8aW8uc2NhbmJv" +
+    "dC5TY2FuYm90U0RLU3dpZnRVSURlbW" +
+    "98aW8uc2NhbmJvdC5zZGtfd3JhcHBl" +
+    "ci5kZW1vLmJhcmNvZGV8aW8uc2Nhbm" +
+    "JvdC5zZGstd3JhcHBlci5kZW1vLmJh" +
+    "cmNvZGV8aW8uc2NhbmJvdC5zZGsuaW" +
+    "50ZXJuYWxkZW1vfGxvY2FsaG9zdHxP" +
+    "cGVyYXRpbmdTeXN0ZW1TdGFuZGFsb2" +
+    "5lfHNjYW5ib3RzZGstcWEtMS5zMy1l" +
+    "dS13ZXN0LTEuYW1hem9uYXdzLmNvbX" +
+    "xzY2FuYm90c2RrLXFhLTIuczMtZXUt" +
+    "d2VzdC0xLmFtYXpvbmF3cy5jb218c2" +
+    "NhbmJvdHNkay1xYS0zLnMzLWV1LXdl" +
+    "c3QtMS5hbWF6b25hd3MuY29tfHNjYW" +
+    "5ib3RzZGstcWEtNC5zMy1ldS13ZXN0" +
+    "LTEuYW1hem9uYXdzLmNvbXxzY2FuYm" +
+    "90c2RrLXFhLTUuczMtZXUtd2VzdC0x" +
+    "LmFtYXpvbmF3cy5jb218c2NhbmJvdH" +
+    "Nkay13YXNtLWRlYnVnaG9zdC5zMy1l" +
+    "dS13ZXN0LTEuYW1hem9uYXdzLmNvbX" +
+    "x3ZWJzZGstZGVtby1pbnRlcm5hbC5z" +
+    "Y2FuYm90LmlvfCoucWEuc2NhbmJvdC" +
+    "5pbwoxNzE5ODc4Mzk5CjgzODg2MDcK" +
+    "MzE=\n";
 
 _initScanbotSdk() async {
   Directory? storageDirectory;
