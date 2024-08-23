182fe9ecf834335b46b87e294286cde69af84fee
Charles Stander <charles@scanbot.io>
2024-08-23 10:30:51 +0200

    Update to Scanbot logo

--- lib/doc_code_snippet/find_and_pick_scanning_usecase_snippet.dart
+++ lib/doc_code_snippet/find_and_pick_scanning_usecase_snippet.dart
@@ -34,13 +34,13 @@ void findAndPickModeUseCaseSnippet() {
         barcodeValue: "123456",
         title: "numeric barcode",
         image:
-            "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
+            "https://avatars.githubusercontent.com/u/1454920",
         count: 4),
     ExpectedBarcode(
         barcodeValue: "SCANBOT",
         title: "value barcode",
         image:
-            "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
+            "https://avatars.githubusercontent.com/u/1454920",
         count: 3)
   ];
 
