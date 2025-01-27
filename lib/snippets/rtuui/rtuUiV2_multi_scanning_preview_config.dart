bbb1a133ee56e663b3c2b45d7017f5dbc068de56
yurii-scanbot <yurii@scanbot.io>
2025-01-27 13:19:01 +0100

    update rtuUiV2MultipleScanningPreviewConfig snippet with MultipleScanningMode

--- lib/snippets/rtuui/rtuUiV2_multi_scanning_preview_config.dart
+++ lib/snippets/rtuui/rtuUiV2_multi_scanning_preview_config.dart
@@ -4,12 +4,28 @@ BarcodeScannerConfiguration rtuUiV2MultipleScanningPreviewConfig() {
   // Create the default configuration object.
   var config = BarcodeScannerConfiguration();
 
+  // Initialize the use case for multiple scanning.
+  var scanningMode = MultipleScanningMode();
+
+  // Set the sheet mode for the barcodes preview.
+  scanningMode.sheet.mode = SheetMode.COLLAPSED_SHEET;
+
+  // Set the height for the collapsed sheet.
+  scanningMode.sheet.collapsedVisibleHeight = CollapsedVisibleHeight.LARGE;
+
+  // Configure the submit button on the sheet.
+  scanningMode.sheetContent.submitButton.text = "Submit";
+  scanningMode.sheetContent.submitButton.foreground.color =
+      ScanbotColor("#000000");
+
   // Configure localization parameters.
   config.localization.barcodeInfoMappingErrorStateCancelButton = 'Custom Cancel title';
   config.localization.cameraPermissionCloseButton = 'Custom Close title';
   // Configure other strings as needed.
 
-  // Configure other parameters as needed.
+   // Configure other parameters, pertaining to multiple-scanning mode as needed.
+
+  config.useCase = scanningMode;
 
   return config;
 }
