9ebc8133fa408be193a42b5d2ad60222469164de
Oleksii <oleksii@scanbot.io>
2026-01-19 15:46:55 +0100

    BS 8.0: Added take picture snippet

---
+++ lib/snippets/take_picture.dart
@@ -0,0 +1,38 @@
+import 'package:barcode_scanner/scanbot_barcode_sdk.dart';
+import 'package:flutter/material.dart';
+
+ScanbotCameraController cameraController = ScanbotCameraController();
+
+Widget buildCameraViewWithTakePictureButton() {
+  return Stack(
+    children: [
+      BarcodeScannerCamera(
+        controller: cameraController,
+        configuration: BarcodeCameraConfiguration(
+          scannerConfiguration: BarcodeClassicScannerConfiguration(),
+          overlayConfiguration: SelectionOverlayScannerConfiguration(),
+        ),
+        takePictureListener: (image) {
+          // Handle take picture event
+        },
+        barcodeListener: (barcodeItems) async {
+          // Handle barcode scanning results
+        },
+      ),
+      Align(
+        alignment: Alignment.bottomCenter,
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: ShutterButtonView(
+            onPressed: () {
+              cameraController.takePicture();
+            },
+            primaryColor: Colors.pink,
+            accentColor: Colors.white,
+            animatedLineStrokeWidth: 2,
+          ),
+        ),
+      )
+    ],
+  );
+}
